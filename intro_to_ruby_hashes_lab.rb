6df9f965cc0b89dc11162559855285a192c3ec47
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-03 21:24:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
   top_transportation_system= {:railroads => 
                                   {:pieces =>4, :rent_in_dollars => 
                                                       {:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned =>200},:names =>
-                                                                          {:reading_railroad =>{}, :pennsyvania_railroad =>{}, :name_3 =>{}, :name_4 =>{}}}}
+                                                                          {:reading_railroad =>{}, :pennsylvania_railroad =>{}, :name_3 =>{}, :name_4 =>{}}}}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
