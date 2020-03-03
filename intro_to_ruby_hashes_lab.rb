b968cabfd12a3ae698d74fb9dd8880aa45199f6e
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-03 21:31:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,6 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  def monopoly_with_third_tier
   top_transportation_system= {:railroads => 
                                   {:pieces =>4, :rent_in_dollars => 
                                                       {:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned =>200},:names =>
